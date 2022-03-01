a72194dc408b975d98a534d893f3777ac8b5df16
evanpascual <pascual.evan@gmail.com>
2022-02-28 23:27:16 -0800

    Keydown listener

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useStore } from "./store";
 import WordRow from "./WordRow";
 import { LETTER_LENGTH } from "./word-utils";
@@ -7,19 +7,7 @@ export const GUESS_LENGTH = 6;
 
 export default function App() {
   const state = useStore();
-  const [guess, setGuess] = useState("");
-
-  //onChange handler for user keyboard input.
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const newGuess = e.target.value;
-
-    if (newGuess.length === LETTER_LENGTH) {
-      state.addGuess(newGuess);
-      setGuess("");
-      return;
-    }
-    setGuess(newGuess);
-  };
+  const [guess, setGuess] = useGuess();
 
   let rows = [...state.rows]; //Each row is a guess. Copy list from state.
 
@@ -37,15 +25,6 @@ export default function App() {
     <div className="mx-auto w-96 relative">
       <header className="border-b border-gray-500 pb-2 my-2">
         <h1 className="text-4xl text-center"> Wordle </h1>
-        <div>
-          <input
-            className="w-1/2 p-2 border-2 border-gray-500"
-            type="text"
-            value={guess}
-            onChange={onChange}
-            disabled={isGameOver}
-          />
-        </div>
       </header>
 
       <main className="grid grid-rows-6 gap-4" aria-label="main-content">
@@ -75,4 +54,63 @@ export default function App() {
       )}
     </div>
   );
+}
+
+//Allow keyboard input without an input field
+function useGuess(): [string, React.Dispatch<React.SetStateAction<string>>] {
+  const addGuess = useStore().addGuess;
+  const [guess, setGuess] = useState("");
+  const previousGuess = usePrevious(guess);
+
+  const onKeyDown = (e: KeyboardEvent) => {
+    let letter = e.key;
+    console.log(letter);
+    setGuess((currentGuess) => {
+      const newGuess =
+        letter.length === 1 ? currentGuess + letter : currentGuess;
+
+      switch (letter) {
+        case "Backspace":
+          return newGuess.slice(0, -1);
+        case "Enter":
+          if (newGuess.length === LETTER_LENGTH) {
+            return "";
+          }
+      }
+
+      if (currentGuess.length === LETTER_LENGTH) {
+        return currentGuess;
+      }
+      return newGuess;
+    });
+  };
+
+  //Keyboard listener.
+  useEffect(() => {
+    document.addEventListener("keydown", onKeyDown);
+    return () => {
+      document.removeEventListener("keydown", onKeyDown);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (guess.length === 0 && previousGuess?.length === LETTER_LENGTH) {
+      addGuess(previousGuess);
+    }
+  }, [guess]);
+
+  return [guess, setGuess];
+}
+
+//From https://usehooks.com/usePrevious/
+function usePrevious<T>(value: T): T {
+  // The ref object is a generic container whose current property is mutable ...
+  // ... and can hold any value, similar to an instance property on a class
+  const ref: any = useRef<T>();
+  // Store current value in ref
+  useEffect(() => {
+    ref.current = value;
+  }, [value]); // Only re-run if value changes
+  // Return previous value (happens before update in useEffect above)
+  return ref.current;
 }
